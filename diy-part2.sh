90e1975e01d6bbbe6ce2f8ee2b19766fe4d95cce
icekale <38102110+icekale@users.noreply.github.com>
2024-09-16 19:42:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,10 +27,6 @@ function git_sparse_clone() {
 # 添加额外插件
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pushbot
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd luci-lib-xterm taskd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-linkease linkease ffmpeg-remux
 
 # 加入OpenClash核心
 chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
@@ -40,17 +36,6 @@ echo "
 # mosdns
 #CONFIG_PACKAGE_luci-app-mosdns=y
 
-# pushbot
-#CONFIG_PACKAGE_luci-app-pushbot=y
-
-# 阿里DDNS
-#CONFIG_PACKAGE_luci-app-aliddns=y
-
-# Jellyfin
-#CONFIG_PACKAGE_luci-app-jellyfin=y
-
-# 易有云
-#CONFIG_PACKAGE_luci-app-linkease=y
 
 " >> .config
 
